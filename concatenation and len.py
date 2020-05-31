52dbf948bcee6cdf458075b400d8c886527798bf
swetha-reddy-03 <66151328+swetha-reddy-03@users.noreply.github.com>
2020-05-31 11:03:24 +0530

    Add files via upload

---
+++ concatenation and len.py
@@ -0,0 +1,10 @@
+#concatenation nd print first nd last letters of two stings
+a = "python"
+b = "java"
+c = a + b
+d = a[0] + a[-1] + b[0] + b[-1]
+print(c)
+print(d)
+print(len(a))
+print(len(b))
+print(len(c))
