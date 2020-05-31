b910c7054288ebf49cd20229a90564d460d49c93
swetha-reddy-03 <66151328+swetha-reddy-03@users.noreply.github.com>
2020-05-31 11:00:09 +0530

    Add files via upload

---
+++ input function.py
@@ -0,0 +1,22 @@
+#input function for data types
+
+a = int(input("enter the integer"))
+print(a)
+print(type(a))
+        
+b = float(input("enter the float value"))
+print(b)
+print(type(b))
+          
+c = input("enter the string")
+print(c)
+print(type(c))
+          
+d = bool(input("enter the boolean value"))
+print(d)
+print(type(d))
+          
+e = a+b
+print(e)
+                  
+    
