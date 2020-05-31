164600229c559be90cd5648ffc4da467398a1757
swetha-reddy-03 <66151328+swetha-reddy-03@users.noreply.github.com>
2020-05-31 11:04:26 +0530

    Add files via upload

---
+++ print function.py
@@ -0,0 +1,11 @@
+# here the use to print function
+print('helo world')
+print("python")
+print('java')
+print("i'm swetha")
+print("today many of people suffering with diseases \"pollution\" is the main reason")
+print('''hi
+hello
+im learning
+python
+''')
