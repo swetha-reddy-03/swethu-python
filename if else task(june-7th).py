15ec857b727272a794f9537225bc01c2423f29f2
swetha-reddy-03 <66151328+swetha-reddy-03@users.noreply.github.com>
2020-06-07 19:05:51 +0530

    Add files via upload

---
+++ if else task(june-7th).py
@@ -0,0 +1,16 @@
+#check whether the num is even or odd
+val1=int(input("enter the first number: "))
+print(val1)
+if val1%2==0:
+    print("the given num is even")
+else:
+    print("the given num is odd")
+print("completed")
+
+#check whether the num is positive or negative
+val2=int(input("enter the second number: "))
+if val2>0:
+    print("the num is positive")
+else:
+    print("the num is negative")
+print("completed")
---
+++ if task(june7th).py
@@ -0,0 +1,12 @@
+#check whether a number 5 or not
+val1=int(input("enter the number: "))
+if val1==5:
+    print("doing comparision")
+    print("the condition is correct")
+print("main")
+
+#check whether a num is greaterthan 3 and lessthan 8
+val2=int(input("enter the first number: "))
+if val2>3 and val2<8:
+   print("the condition is true")
+print("main")
